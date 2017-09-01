12d6c28ebe629b85cf1d8c42fda7a2516639ee27
LIUWENCAN <pheye@qq.com>
2017-09-01 19:50:07 +0800

    完成<The Separation Of Concerns>

--- src/Calendar/Controller/LeapYearController.php
+++ src/Calendar/Controller/LeapYearController.php
@@ -3,18 +3,13 @@ namespace Calendar\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Calendar\Model\LeapYear;
 
-function is_leap_year($year = null) {
-    if (null === $year) {
-        $year = date('Y');
-    }
-
-    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
-}
 
 class LeapYearController {
     public function indexAction(Request $request, $year) {
-        if (is_leap_year($year)) {
+        $leapYear = new LeapYear();
+        if ($leapYear->isLeapYear($year)) {
             return new Response("Yes, this is a leap year");
         }
         return new Response("No, this is not a leap year");
